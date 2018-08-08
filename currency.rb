d9b36d60560a99c5ab83a62b21a9d96bf6a5819e
frisaac <frisaac@stu.naperville203.org>
2018-08-08 16:34:01 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,28 @@
-# Write your cod here
+# Write your code here
+def usd_to_eur(dollar_amt = 1.2)
+  dollar_amt/1.2
+end
+puts usd_to_eur
+
+def eur_to_usd(eur_amt = 1)
+  eur_amt*1.2
+end
+puts eur_to_usd
+
+def usd_to_eur
+end
+
+def usd_to_gbp
+end
+
+def usd_to_aud
+end
+
+def jpy_to_usd
+end
+
+def gbp_to_usd
+end
+
+def aud_to_usd
+end
